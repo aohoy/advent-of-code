1b8cb84c1fda66c6aebfa38d6d291852ebc9a5c9
aohoyd <gh@aohoy.dev>
2023-12-02 19:13:15 +0300

    Day 02

---
+++ 2023/.day_template/src/part2.rs
@@ -0,0 +1,18 @@
+use std::error::Error;
+
+pub fn solve(_input: &str) -> Result<String, Box<dyn Error>> {
+    todo!("Implement solution")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve() -> Result<(), Box<dyn Error>> {
+        todo!("Implement test")
+        let input = "";
+        assert_eq!("", solve(input)?);
+        Ok(())
+    }
+}
---
+++ 2023/day_02/src/bin/part2.rs
@@ -0,0 +1,9 @@
+use day_02::part2::solve;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = include_str!("../../input2.txt");
+    let result = solve(file)?;
+    println!("{}", result);
+    Ok(())
+}
