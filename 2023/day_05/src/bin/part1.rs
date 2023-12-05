9d731783a0ce3e76a1d8437b197bbaece0e42eae
aohoyd <gh@aohoy.dev>
2023-12-05 17:38:48 +0300

    Day 05

---
+++ 2023/day_05/src/bin/part1.rs
@@ -0,0 +1,9 @@
+use day_05::part1::solve;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = include_str!("../../input1.txt");
+    let result = solve(file)?;
+    println!("{}", result);
+    Ok(())
+}
